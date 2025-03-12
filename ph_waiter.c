887ff54a291b16128221aae8d8516d18d254d36d
Ismail <iboukhss.student@42luxembourg.lu>
2025-03-12 10:56:05 +0100

    norm: refactor waiter routine

--- ph_waiter.c
+++ ph_waiter.c
@@ -6,7 +6,7 @@
 /*   By: iboukhss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 21:06:31 by iboukhss          #+#    #+#             */
-/*   Updated: 2025/03/11 21:15:52 by iboukhss         ###   ########.fr       */
+/*   Updated: 2025/03/11 23:50:57 by iboukhss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,64 +14,70 @@
 
 #include <unistd.h>
 
+static void	process_wait_queue(t_simulation *sim)
+{
+	t_philosopher	*wait;
+
+	while (1)
+	{
+		wait = peek(&sim->wait_queue);
+		if (!wait)
+		{
+			return ;
+		}
+		if (neighbors_are_eating(wait))
+		{
+			return ;
+		}
+		else
+		{
+			pthread_mutex_lock(&wait->state_lock);
+			wait->state = EATING;
+			pthread_mutex_unlock(&wait->state_lock);
+			dequeue(&sim->wait_queue);
+		}
+	}
+}
+
+static void	process_request_queue(t_simulation *sim)
+{
+	t_philosopher	*req;
+	t_philosopher	*wait;
+	t_philosopher	*new_wait;
+
+	req = peek(&sim->req_queue);
+	if (req)
+	{
+		wait = peek(&sim->wait_queue);
+		if (wait && (req == wait->left || req == wait->right))
+		{
+			new_wait = dequeue(&sim->req_queue);
+			enqueue(&sim->wait_queue, new_wait);
+		}
+		else if (neighbors_are_eating(req))
+		{
+			new_wait = dequeue(&sim->req_queue);
+			enqueue(&sim->wait_queue, new_wait);
+		}
+		else
+		{
+			pthread_mutex_lock(&req->state_lock);
+			req->state = EATING;
+			pthread_mutex_unlock(&req->state_lock);
+			dequeue(&sim->req_queue);
+		}
+	}
+}
+
 void	*waiter_routine(void *arg)
 {
 	t_simulation	*sim;
-	t_philosopher	*wait;
-	t_philosopher	*new_wait;
-	t_philosopher	*req;
 
 	sim = (t_simulation *)arg;
 	while (simulation_is_running(sim))
 	{
-		// Check the wait queue first
-		while (1)
-		{
-			wait = peek(&sim->wait_queue);
-
-			if (!wait)
-			{
-				break ;
-			}
-
-			// Ping pong back and forth with the request and wait queue
-			if (neighbors_are_eating(wait))
-			{
-				break ;
-			}
-			else
-			{
-				pthread_mutex_lock(&wait->state_lock);
-				wait->state = EATING;
-				pthread_mutex_unlock(&wait->state_lock);
-				dequeue(&sim->wait_queue);
-			}
-		}
-
-		// Check the request queue right after
-		req = peek(&sim->req_queue);
-
-		if (req)
-		{
-			// Special case, requested item is a neighbor of #1 in wait queue
-			if (wait && (req == wait->left || req == wait->right))
-			{
-				new_wait = dequeue(&sim->req_queue);
-				enqueue(&sim->wait_queue, new_wait);
-			}
-			else if (neighbors_are_eating(req))
-			{
-				new_wait = dequeue(&sim->req_queue);
-				enqueue(&sim->wait_queue, new_wait);
-			}
-			else
-			{
-				pthread_mutex_lock(&req->state_lock);
-				req->state = EATING;
-				pthread_mutex_unlock(&req->state_lock);
-				dequeue(&sim->req_queue);
-			}
-		}
+		process_wait_queue(sim);
+		process_request_queue(sim);
 		usleep(500);
 	}
 	return (NULL);
